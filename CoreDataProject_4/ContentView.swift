6b3a8edebeec67b82d4a1411f8f3b177e36550ea
Subhrajyoti <subha.ionic3@gmail.com>
2020-08-06 12:07:41 +0530

    Initial Commit

---
+++ CoreDataProject_4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataProject_4
+//
+//  Created by Subhrajyoti Chakraborty on 06/08/20.
+//  Copyright © 2020 Subhrajyoti Chakraborty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
